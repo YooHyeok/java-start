2db886ae28bdb48af3b8a32992b41bbbaf1707ee
YooHyeok <yjk7454@naver.com>
2025-07-05 22:14:46 +0900

    feat: `7.주석(comment)` - 한줄 주석, 여러줄 주석

---
+++ src/CommentJava.java
@@ -0,0 +1,10 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // hello java1을 출력합니다. (한 줄 주석 - 부분 적용)
+//        System.out.println("Hello Java2"); 한줄 주석 - 라인 전체 적용
+        /* 여러 줄 주석
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+        */
+    }
+}
